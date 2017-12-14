c620293431b07fed23bbc7c3c4c1f68baabe840d
EXXETA-DE\jogr1 <josip.grabant@exxeta.com>
2017-12-14 22:46:09 +0100

    uebung 2 lisp

---
+++ lisp/uebung2.lisp
@@ -0,0 +1,98 @@
+;;;; uebung 2 
+;;; insert tree val
+;; value is added to the tree
+
+(defun insert (tree val)
+  (cond (
+        (null tree) (append (list val)))
+        ((< val (first tree)) (append (list (first tree) (second tree) (insert (third tree) val))))
+        ((> val (first tree)) (append (list (first tree) (insert (second tree) val) (third tree))))
+        (T (append (list (first tree) (second tree) (third tree))))
+  )
+)
+
+;; insert from file
+;; value is added from file into tree
+(defun read-file (filename)
+    (with-open-file(stream filename) ...)) ;; TBD
+
+;; contains tree val
+;; checks if value exists in tree
+(defun contains (tree val)
+  (if (endp tree) nil
+  (or (equal val (first tree))
+  (or (contains (second tree) val) (contains (third tree) val))))
+)
+
+;;size tree
+;; returnes the size of the three
+(defun size (tree)
+  (cond ((null tree) 0)
+        ((listp (first tree)) (+ (size (first tree)) (size (rest tree))))
+        (T (+ 1 (size (rest tree))))
+  )
+)
+
+;;height tree
+;; returns the height of a tree
+(defun height (tree)
+  (if (endp tree)
+    0
+    (+ 1 (max (height (second tree)) (height (third tree))))
+  )
+)
+
+;;getMax tree
+;; returns the hightest value from the tree
+(defun getMax (tree)
+  (if (endp tree)
+  0
+  (max (first tree) (max (getMax (second tree)) (getMax (third tree))))
+  )
+)
+
+;;getMin tree
+;; returns the min value from the tree
+
+(defun getMin (tree)
+  (if (endp tree)
+  0
+  (min (first tree) (min (getMin (second tree)) (getMin (third tree))))
+  )
+)
+
+;; isEmpty tree
+;; returns true if the tree is empty
+
+(defun isEmpty (tree)
+  (if (null tree) T NIL)
+)
+
+;; print levelorder
+;; prints the levelorder fom the tree
+(defun currentLevel (tree level)
+    (if (endp tree)
+    0
+    (cond ((= 1 level)  (list (first tree)))
+        ((append (currentLevel (second tree) (1- level))
+        (currentLevel (third tree) (1- level))))
+    )
+    )
+)
+    
+(defun printLevelorder (tree)
+    (setq h (height tree))
+    (setq i 1)
+    (setq r (list (first tree)))
+    (loop
+    (setq i  (+ i 1))
+    (setq r (append r (currentLevel tree i)))
+    (when (= i h) (return r))
+    )
+)
+
+;; tree for test purposes '(1 (2 (3 4))(5(6)))
+
+(setq tree '(1 (2 (3)(4))(5(6))))
+
+(insert tree 9)
